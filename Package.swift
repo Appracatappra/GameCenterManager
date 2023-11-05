b985b107de2d9cd3d2e44d2cea0dbbf7b40aea43
Kevin Mullins <kevin_mullins@appracatappra.com>
2023-11-05 04:36:07 -0600

    Build
    Build

--- Package.swift
+++ Package.swift
@@ -17,16 +17,16 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/Appracatappra/LogManager", .upToNextMajor(from: "1.0.1")),
         .package(url: "https://github.com/Appracatappra/SwiftletUtilities", .upToNextMajor(from: "1.1.1")),
-        .package(url: "https://github.com/SwiftPackageRepository/GameKitUI.swift.git", .upToNextMajor(from: "0.5.1")),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "GameCenterManager",
-            dependencies: ["LogManager", "SwiftletUtilities", "GameKitUI"]),
+            dependencies: ["LogManager", "SwiftletUtilities"]),
         .testTarget(
             name: "GameCenterManagerTests",
             dependencies: ["GameCenterManager"]),
     ]
 )
+
